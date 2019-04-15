199903fe335f93a2355b45e45c54be5cdcb93b75
Away0x <wutong0910@foxmail.com>
2019-04-15 02:44:02 +0000

    修复 N+1 问题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,9 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+        // $topics = Topic::paginate(30); 有 n+1 问题
+
+        $topics = Topic::with('user', 'category')->paginate(30);
 		return view('topics.index', compact('topics'));
 	}
 
