99c4148a579c4e1a13e0235ce9604dc2488ba473
Away0x <wutong0910@foxmail.com>
2019-04-15 02:53:00 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -4,3 +4,19 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+function category_nav_active($category_id)
+{
+    /**
+     * active_class 函数是 hieu-le/active 提供的
+     *
+     * 如果 $condition 不为 True 即会返回字符串 `active`
+     *
+     * @param        $condition
+     * @param string $activeClass
+     * @param string $inactiveClass
+     *
+     * @return string
+     */
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Auth::routes(['verify' => true]);
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
