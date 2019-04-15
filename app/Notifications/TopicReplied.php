4db9c96b26ecf97f46bf376ef68db0f3cb1917e8
Away0x <wutong0910@foxmail.com>
2019-04-15 07:27:07 +0000

    支持队列的邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -10,7 +10,10 @@ use Illuminate\Notifications\Messages\MailMessage;
 use App\Models\Reply;
 
 
-class TopicReplied extends Notification
+// class TopicReplied extends Notification
+
+// 使用队列发送通知
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -25,7 +28,7 @@ class TopicReplied extends Notification
     public function via($notifiable)
     {
         // 开启通知的频道
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     public function toDatabase($notifiable)
@@ -44,5 +47,14 @@ class TopicReplied extends Notification
             'topic_id' => $topic->id,
             'topic_title' => $topic->title,
         ];
+    }
+
+    public function toMail($notifiable)
+    {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
+        return (new MailMessage)
+                    ->line('你的话题有新回复！')
+                    ->action('查看回复', $url);
     }
 }
