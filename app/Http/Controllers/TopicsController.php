831d65fda9cc1a619af621bdf4dc3d1a6f9438d8
Away0x <wutong0910@foxmail.com>
2019-04-15 03:43:06 +0000

    话题排序

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,11 +14,11 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request, Topic $topic)
 	{
         // $topics = Topic::paginate(30); 有 n+1 问题
 
-        $topics = Topic::with('user', 'category')->paginate(30);
+        $topics = $topic->withOrder($request->order)->paginate(20);
 		return view('topics.index', compact('topics'));
 	}
 
